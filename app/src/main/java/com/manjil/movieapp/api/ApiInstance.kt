d1d7dcc82a17bba9afbfb3f3b3101f4b02ff72b5
manjil <manjilkoju09@gmail.com>
2023-09-15 15:55:10 +0545

    tabLayout gravity fixed

--- app/src/main/java/com/manjil/movieapp/api/ApiInstance.kt
+++ app/src/main/java/com/manjil/movieapp/api/ApiInstance.kt
@@ -6,7 +6,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object ApiInstance {
-    //    private const val BASE_URL = "https://weatherbit-v1-mashape.p.rapidapi.com/"
+//    private const val BASE_URL = "https://weatherbit-v1-mashape.p.rapidapi.com/"
     private const val BASE_URL = "http://10.0.2.2/manjilKoju/"
     private var retrofit: Retrofit? = null
 
